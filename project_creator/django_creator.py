449459fd93e7539b4597ddb09ef006ae849a272f
Danillo <danillordm@gmail.com>
2024-09-11 21:42:46 -0300

    Refactor DjangoCreator

--- project_creator/django_creator.py
+++ project_creator/django_creator.py
@@ -2,16 +2,17 @@ from project_creator.project_creator import ProjectCreator
 
 
 class DjangoCreator(ProjectCreator):
-
     def _create_project(self) -> bool:
-        checker = False
         try:
             if not self._venv.check_library("django"):
                 self._venv.install_library("django")
 
-            if (self._venv.execute_venv_command(f"python -m django startproject {self._project_name}")):
-                checker = True
+            command = f"python -m django startproject {self._project_name}"
+            if self._venv.execute_venv_command(command):
+                return True
+            else:
+                print(f"Failed to create Django project '{self._project_name}'.")
+                return False
         except Exception as e:
-            print(f"Erro ao criar o projeto django {self._project_name}: {e}")
-
-        return checker
+            print(f"Error creating Django project '{self._project_name}': {e}")
+            return False
