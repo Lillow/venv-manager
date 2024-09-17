6d0c33206b8b3e96ff47c029836b324152156ba5
Danillo <danillordm@gmail.com>
2024-09-16 22:11:51 -0300

    Add terminal_utils.py

---
+++ utils/terminal_utils.py
@@ -0,0 +1,20 @@
+import os
+import msvcrt
+
+
+def clean_screen() -> None:
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def wait_for_keypress() -> None:
+    print("\nPress any key to continue...")
+
+    try:
+        msvcrt.getch()
+    except Exception as e:
+        print(f"Error waiting for key press: {e}")
+
+
+def pause_and_clear() -> None:
+    wait_for_keypress()
+    clean_screen()
