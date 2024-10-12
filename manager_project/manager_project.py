9df0375aef8af530769dcaaa594d5f589a2ccf43
Danillo <danillordm@gmail.com>
2024-10-12 15:55:11 -0300

    Add manager_project/manager_project.py

---
+++ manager_project/manager_project.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+from manager.manager import Manager
+from manager_venv.manager_venv import ManagerVenv
+
+
+class ManagerProject(Manager, metaclass=ABCMeta):
+    def __init__(self, venv: ManagerVenv, project_name: str) -> None:
+        self.__venv: ManagerVenv = venv
+        super().__init__(project_name)
+
+    def _create(self) -> bool:
+        return self._create_project()
+
+    @abstractmethod
+    def _create_project(self) -> bool:
+        pass
+
+    @property
+    def _venv(self) -> ManagerVenv:
+        return self.__venv
+
+    @property
+    def _project_name(self) -> str:
+        return self.__project_name
