7fbf89c5e3c3b481aea303b1dfcb63c2748e882b
Danillo <danillordm@gmail.com>
2024-10-12 23:46:23 -0300

    Add docstring in ManagerProject

--- manager_project/manager_project.py
+++ manager_project/manager_project.py
@@ -4,17 +4,41 @@ from manager_venv.manager_venv import ManagerVenv
 
 
 class ManagerProject(Manager, metaclass=ABCMeta):
+    """Abstract base class for managing projects with virtual environments.
+
+    This class extends the Manager class to handle project-specific tasks,
+    such as project directory creation and managing virtual environments.
+
+    Attributes:
+    __venv (ManagerVenv): The virtual environment manager for the project.
+    """
+
     def __init__(self, venv: ManagerVenv, project_name: str) -> None:
-        self.__venv: ManagerVenv = venv
+        """Initialize the ManagerProject with a virtual environment and project name.
+
+        Args:
+            venv (ManagerVenv): The ManagerVenv instance for managing the project's virtual environment.
+            project_name (str): The name of the project.
+        """
+        self._venv: ManagerVenv = venv
         super().__init__(project_name)
 
     def _create(self) -> bool:
+        """Create the project by calling the _create_project method.
+
+        Returns:
+            bool: True if the project was created successfully, False otherwise.
+        """
         return self._create_project()
 
     @abstractmethod
     def _create_project(self) -> bool:
-        pass
+        """Abstract method to handle project creation.
 
-    @property
-    def _venv(self) -> ManagerVenv:
-        return self.__venv
+        This method must be implemented by subclasses to define the steps
+        for creating the project.
+
+        Returns:
+            bool: True if the project was created successfully, False otherwise.
+        """
+        pass
