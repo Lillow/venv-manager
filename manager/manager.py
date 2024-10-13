78d1f5b967f66524b0b9227f363df06a29f13f58
Danillo <danillordm@gmail.com>
2024-10-12 22:35:21 -0300

    Remove unused rows, rename attributes and change methods

--- manager/manager.py
+++ manager/manager.py
@@ -1,38 +1,29 @@
-from ast import Return
 from pathlib import Path
 from abc import ABCMeta, abstractmethod
-import os
 
 
 class Manager(metaclass=ABCMeta):
+
     def __init__(self, name: str) -> None:
-        self.__name: str = name.strip().replace(" ", "_")
-        self.__base_dir: str = os.getcwd()
-        self.__dir_path: str = f"{self.__base_dir}\\{self.__str__()}"
-        self.__is_created: bool = self._create()
+        self._name: str = name.strip().replace(" ", "_")
+        self._base_dir: Path = Path.cwd()
+        self._dir_path: Path = self._base_dir / self.__str__()
+        self._is_created: bool = self._create()
 
     @abstractmethod
     def _create(self) -> bool:
         pass
 
-    @property
-    def _dir_path(self) -> str:
-        return self.__dir_path
-
-    @property
-    def _is_created(self) -> bool:
-        return self.__is_created
-
     def _exists_dir(self) -> bool:
-        return os.path.exists(self.__str__())
+        return self._dir_path.exists()
 
     def _create_directories(self, directories: list[str]) -> None:
         for directory in directories:
-            dir_path = Path(self._dir_path) / directory
+            dir_path = self._dir_path / directory
             dir_path.mkdir(parents=True, exist_ok=True)
 
     def _create_file(self, file_name: str, file_content: str) -> None:
-        (Path(self._dir_path) / file_name).write_text(file_content)
+        (self._dir_path / file_name).write_text(file_content)
 
     def __str__(self) -> str:
-        return self.__name
+        return self._name
