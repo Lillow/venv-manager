9f772689ce86f356cc478197f7edd000cabec364
Danillo <danillordm@gmail.com>
2024-10-20 20:30:40 -0300

    Add options_project.py

---
+++ menu/menu_project/options_project.py
@@ -0,0 +1,34 @@
+from manager_venv.manager_venv import ManagerVenv
+from manager_project.manager_django import ManagerDjango
+from manager_project.manager_flask import ManagerFlask
+from utils.terminal_utils import pause_and_clear, clean_screen
+
+
+def create_project(
+    venv: ManagerVenv, project_name: str
+) -> ManagerDjango | ManagerFlask:
+    while True:
+        clean_screen()
+        print(
+            """
+𝙋𝙧𝙤𝙟𝙚𝙘𝙩 𝙏𝙮𝙥𝙚
+
+    1 - Django project
+    2 - Flask project
+    """
+        )
+        option: str = input("\nChoose an option: ")
+        match option:
+            case "1":
+                return ManagerDjango(venv, project_name)
+                _create_and_clear()
+                break
+            case "2":
+                return ManagerFlask(venv, project_name)
+                _create_and_clear()
+                break
+
+
+def _create_and_clear() -> None:
+    pause_and_clear()
+    print("Creating or find project...\n")
