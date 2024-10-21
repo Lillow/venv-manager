2075abf655779cc558d8cb27bf3556d879a1fef0
Danillo <danillordm@gmail.com>
2024-10-20 21:18:30 -0300

    Add print_banner() in MenuInitial

--- menu_organized/menu_initial.py
+++ menu_organized/menu_initial.py
@@ -4,4 +4,5 @@ from manager_menu.manager_menu import ManagerMenu
 class MenuInitial(ManagerMenu):
     def __init__(self, banner: str, msg_manager: str) -> None:
         super().__init__(banner)
+        self.print_banner()
         self.manager_name: str = input(msg_manager)
